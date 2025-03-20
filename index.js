e3c13aff6097402abb9fd22ca8974172786138fd
atgdot <ayushraj9868@gmail.com>
2025-03-20 21:40:40 +0530

    debug

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 
 // Middleware
